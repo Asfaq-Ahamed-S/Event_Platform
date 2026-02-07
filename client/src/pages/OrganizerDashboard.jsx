e342f4ae225ed507ea85151bd42b2186dd148936
Asfaq Ahamed <asfaqahamed6239@gmail.com>
2026-02-07 11:14:47 +0530

    Added EventCreation Feature on Organizer Dashboard

--- client/src/pages/OrganizerDashboard.jsx
+++ client/src/pages/OrganizerDashboard.jsx
@@ -5,6 +5,10 @@ export default function UserDashboard() {
     //State variables
     const [events, setEvents] = useState([]);
     const [error, setError] = useState("");
+    const [title, setTitle] = useState("");
+    const [date, setDate] = useState("");
+    const [location, setLocation] = useState("");
+    const [showForm, setShowForm] = useState(false); //toggle form visibilty
 
     useEffect(()=> {
         const fetchOrganizerEvents = async () => {
@@ -29,15 +33,84 @@ export default function UserDashboard() {
 
         fetchOrganizerEvents();
     }, []);
+
+    //Handle event creation
+    const handleCreateEvent = async (e) => {
+        e.preventDefault();
+        try {
+            const API_URL= import.meta.env.VITE_API_URL;
+            const token = localStorage.getItem("token");
+
+            const res = await axios.post(
+                `${API_URL}/events`,
+                {title, date, location},
+                {headers: {Authorization: `Bearer ${token}`}}
+            );
+
+            console.log("Create Event response:", res.data);
+
+            //Add new event to state instantly
+            setEvents([...events, res.data]); //backend returns the event directly
+            setTitle("");
+            setDate("");
+            setLocation("");
+            setShowForm(false); //hide form after creation
+        } catch (err) {
+            console.log(err.response?.data);
+            setError(err.response?.data?.error || "Failed to create event");
+        }
+    };
     
     return (
         <div className="container mt-5">
-            <h2 className="mb-4">Organizer Dashboard</h2>
+            <h2 className="mb-3">Organizer Dashboard</h2>
             {error && <div className="alert alert-danger">{error}</div>}
+
+            {/* Toggle button */}
+            {!showForm && (
+                <button className="btn btn-success mb-3" onClick={()=> setShowForm(true)}>
+                    Create Event
+                </button>
+            )}
+
+            {/* Event Creation Form (when showForm=true) */}
+            {showForm && (
+                <form onSubmit={handleCreateEvent} className="mb-4">
+                    <div className="mb-3">
+                        <label className="form-label">Event Title</label>
+                        <input type="text" className="form-control" value={title} onChange={
+                            (e)=>setTitle(e.target.value)
+                        } required />
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Event Date</label>
+                        <input type="date" className="form-control" value={date} onChange={
+                            (e)=>setDate(e.target.value)
+                        } required />
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Event Location</label>
+                        <input type="text" className="form-control" value={location} onChange={
+                            (e)=>setLocation(e.target.value)
+                        } required />
+                    </div>
+                    
+                    <button type="submit" className="btn btn-primary">
+                        Create
+                    </button>
+                    <button type="button" className="btn btn-secondary ms-2" onClick={
+                        (e)=> setShowForm(false)
+                    }>Cancel</button>
+                </form>
+            )}
+
+            {/* Event List */}
             {events.length > 0 ? (
                 <ul className="list-group">
                     {events.map((event)=>(
-                        <li key={event.id} className="list-group-item">
+                        <li key={event.id || event._id || index} className="list-group-item">
                             <strong>{event.title}</strong> - {new Date(event.date).toLocaleDateString("en-GB",{
                                 day: "numeric",
                                 month: "long",
